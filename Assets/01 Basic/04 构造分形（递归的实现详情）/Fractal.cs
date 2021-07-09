94408903279eca909e0cfda923d984667a37ad4f
Huang huajian <huanghj@zhongbosoft.com>
2021-07-09 18:30:48 +0800

    daily commit 2021/07/09 18:30

--- Assets/01 Basic/04 构造分形（递归的实现详情）/Fractal.cs
+++ Assets/01 Basic/04 构造分形（递归的实现详情）/Fractal.cs
@@ -4,46 +4,86 @@ using UnityEngine;
 
 public class Fractal : MonoBehaviour {
 
-    public Mesh mesh;
+    public Mesh[] meshs;
     public Material material;
     public int maxDepth;
     public float childScale;
+    public float spawnProbability;
+    public float maxRotationSpeed;
+    public float maxTwist;
 
     private int depth;
     private static Vector3[] childDirections = {
-
+        Vector3.up,
+        Vector3.right,
+        Vector3.left,
+        Vector3.forward,
+        Vector3.back
     };
+    private static Quaternion[] childOrientations = {
+        Quaternion.identity,
+        Quaternion.Euler(0f, 0f, -90f),
+        Quaternion.Euler(0f, 0f, 90f),
+        Quaternion.Euler(90f, 0f, 0f),
+        Quaternion.Euler(-90f, 0f, 0f)
+    };
+    private Material[,] materials;
+    private float rotationSpeed;
+
+    private void InitializeMaterials() {
+        materials = new Material[maxDepth + 1, 2];
+        for (int i = 0; i <= maxDepth; i++) {
+            float t = i / (maxDepth - 1f);
+            t *= t;
+            materials[i, 0] = new Material(material);
+            materials[i, 0].color = Color.Lerp(Color.white, Color.yellow, t);
+            materials[i, 1] = new Material(material);
+            materials[i, 1].color = Color.Lerp(Color.white, Color.cyan, t);
+        }
+        materials[maxDepth, 0].color = Color.magenta;
+        materials[maxDepth, 1].color = Color.red;
+    }
 
     void Start() {
-        gameObject.AddComponent<MeshFilter>().mesh = mesh;
-        gameObject.AddComponent<MeshRenderer>().material = material;
+        if (materials == null) {
+            InitializeMaterials();
+        }
+        gameObject.AddComponent<MeshFilter>().mesh = meshs[Random.Range(0, meshs.Length)];
+        gameObject.AddComponent<MeshRenderer>().material = materials[depth, Random.Range(0, 2)];
+        rotationSpeed = Random.Range(-maxRotationSpeed, maxRotationSpeed);
+        transform.Rotate(Random.Range(-maxTwist, maxTwist), 0f, 0f);
         if (depth < maxDepth) {
             StartCoroutine(CreateChildren());
         }
     }
 
     private IEnumerator CreateChildren() {
-        yield return new WaitForSeconds(0.5f);
-        new GameObject("Fractal Child").AddComponent<Fractal>()
-            .Initialize(this, Vector3.up, Quaternion.identity);
-        yield return new WaitForSeconds(0.5f);
-        new GameObject("Fractal Child").AddComponent<Fractal>()
-            .Initialize(this, Vector3.right, Quaternion.Euler(new Vector3(0f, 0f, -90f)));
-        yield return new WaitForSeconds(0.5f);
-        new GameObject("Fractal Child").AddComponent<Fractal>()
-            .Initialize(this, Vector3.left, Quaternion.Euler(new Vector3(0f, 0f, 90f)));
+        for (int i = 0; i < childDirections.Length; i++) {
+            if (Random.value < spawnProbability) {
+                yield return new WaitForSeconds(Random.Range(0.1f, 0.5f));
+                new GameObject("Fractal Child").AddComponent<Fractal>()
+                    .Initialize(this, i);
+            }
+        }
     }
 
-    private void Initialize(Fractal parent, Vector3 direction, Quaternion orientation) {
-        mesh = parent.mesh;
-        material = parent.material;
+    private void Initialize(Fractal parent, int childIndex) {
+        meshs = parent.meshs;
+        materials = parent.materials;
         maxDepth = parent.maxDepth;
         depth = parent.depth + 1;
         childScale = parent.childScale;
+        spawnProbability = parent.spawnProbability;
+        rotationSpeed = parent.rotationSpeed;
+        maxTwist = parent.maxTwist;
         transform.parent = parent.transform;
         transform.localScale = Vector3.one * childScale;
-        transform.localPosition = direction * (0.5f + 0.5f * childScale);
-        transform.localRotation = orientation;
+        transform.localPosition = childDirections[childIndex] * (0.5f + 0.5f * childScale);
+        transform.localRotation = childOrientations[childIndex];
+    }
+
+    private void Update() {
+        transform.Rotate(0f, 30f * Time.deltaTime, 0f);
     }
 
 }
