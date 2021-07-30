a19f5e297dc3394b44d3fa26e82781b832c3c12b
Huang huajian <huanghj@zhongbosoft.com>
2021-07-30 18:34:51 +0800

    daily commit 2021/07/30 18:34

---
+++ Assets/1.1 基础/05 每秒帧率（测试性能）/FPSCounter.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSCounter : MonoBehaviour {
+
+    public int frameRange = 60;
+
+    int[] fpsBuffer;
+    int fpsBufferIndex;
+
+    public int AverageFPS {
+        get; private set;
+    }
+    public int HighestFPS {
+        get; private set;
+    }
+    public int LowestFPS {
+        get; private set;
+    }
+
+    void InitializeBuffer() {
+        if (frameRange <= 0) {
+            frameRange = 1;
+        }
+        fpsBuffer = new int[frameRange];
+        fpsBufferIndex = 0;
+    }
+
+    void UpdateBuffer() {
+        fpsBuffer[fpsBufferIndex++] = (int)(1f / Time.unscaledDeltaTime);
+        if (fpsBufferIndex >= frameRange) {
+            fpsBufferIndex = 0;
+        }
+    }
+
+    void CalculateFPS() {
+        int sum = 0;
+        int highest = 0;
+        int lowest = int.MaxValue;
+        for (int i = 0; i < fpsBuffer.Length; i++) {
+            sum += fpsBuffer[i];
+            if (fpsBuffer[i] > highest) {
+                highest = fpsBuffer[i];
+            }
+            if (fpsBuffer[i] < lowest) {
+                lowest = fpsBuffer[i];
+            }
+        }
+        AverageFPS = sum / frameRange;
+        HighestFPS = highest;
+        LowestFPS = lowest;
+    }
+
+    private void Update() {
+        if (fpsBuffer == null || fpsBuffer.Length != frameRange) {
+            InitializeBuffer();
+        }
+        UpdateBuffer();
+        CalculateFPS();
+    }
+}
---
+++ Assets/1.1 基础/05 每秒帧率（测试性能）/FPSDisplay.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(FPSCounter))]
+public class FPSDisplay : MonoBehaviour {
+
+    [System.Serializable]
+    private struct FPSColor {
+        public Color color;
+        public int minimunFPS;
+    }
+
+    public Text highestFPSLabel;
+    public Text averageFPSLabel;
+    public Text lowestFPSLabel;
+    [SerializeField]
+    private FPSColor[] coloring;
+
+    FPSCounter fpsCounter;
+
+    private void Awake() {
+        fpsCounter = GetComponent<FPSCounter>();
+    }
+
+    private void Update() {
+        Display(averageFPSLabel, fpsCounter.AverageFPS);
+        Display(highestFPSLabel, fpsCounter.HighestFPS);
+        Display(lowestFPSLabel, fpsCounter.LowestFPS);
+    }
+
+    void Display(Text label, int fps) {
+        label.text = Mathf.Clamp(fps, 0, 99).ToString();
+        for (int i = 0; i < coloring.Length; i++) {
+            if (fps >= coloring[i].minimunFPS) {
+                label.color = coloring[i].color;
+                break;
+            }
+        }
+    }
+
+}
